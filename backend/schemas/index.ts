52af7a0a1d788edca8fc11f96fbab34e50ee7863
Imran Yafith <imranyafith@gmail.com>
2023-11-06 13:21:02 -0500

    Deleted all frontend code to restart it

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,7 +1,6 @@
-import about from './about'
 import education from './education'
 import projects from './projects'
 import skills from './skills'
 import work from './work'
 
-export const schemaTypes = [about, work, skills, projects, education]
+export const schemaTypes = [work, skills, projects, education]
--- backend/schemas/skills.ts
+++ backend/schemas/skills.ts
@@ -16,6 +16,12 @@ export default {
       type: 'text',
     },
     {
+      name: 'level',
+      title: 'Level',
+      description: 'Level of skill',
+      type: 'string',
+    },
+    {
       name: 'image',
       title: 'Image',
       type: 'image',

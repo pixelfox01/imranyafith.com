c3a019d78e4af44caa262c8f740b3ad08d5c3b33
Imran Yafith <imranyafith@gmail.com>
2023-11-08 22:48:00 -0500

    Fixed layout issue on mobile landscape mode

--- frontend/app/Containers/About/about.tsx
+++ frontend/app/Containers/About/about.tsx
@@ -1,18 +1,13 @@
-import { Roboto_Mono } from "next/font/google";
 import Details from "./Details/details";
 import Skills from "./Skills/skills";
 
-const roboto_mono = Roboto_Mono({
-  subsets: ["latin"],
-});
-
 const About = () => {
   return (
-    <div className="h-screen lg:flex text-secondary-content ">
-      <div className="bg-base-300 h-1/2 lg:h-full lg:w-1/2 flex flex-col justify-center px-12 lg:pl-20 gap-6">
+    <div className="h-[44rem] lg:flex text-secondary-content ">
+      <div className="bg-base-300 h-1/2 lg:h-full lg:w-1/2 flex flex-col justify-center p-12 lg:pl-20 gap-6">
         <Details />
       </div>
-      <div className="bg-base-200 h-1/2 lg:h-full lg:w-1/2">
+      <div className="bg-base-200 h-1/2 lg:h-full lg:w-1/2 py-12">
         <Skills />
       </div>
     </div>

bdaa924c59892e2726f02aabc0e73e7fab1e9990
Imran Yafith <imranyafith@gmail.com>
2023-11-08 22:06:12 -0500

    Changed design in details page for mobile screens

--- frontend/app/Containers/About/Details/details.tsx
+++ frontend/app/Containers/About/Details/details.tsx
@@ -8,8 +8,8 @@ const roboto_mono = Roboto_Mono({
 
 const Details = () => {
   return (
-    <section className="flex flex-col gap-4 border-l-4 pl-4 border-primary-focus">
-      <div>
+    <section className="flex flex-col gap-4 lg:border-l-4 pl-4 border-primary-focus">
+      <div className="border-l-2 border-primary-focus pl-4 lg:border-none lg:p-0">
         <h1 className="lg:text-4xl text-2xl font-semibold uppercase tracking-widest">
           Imran Yafith
         </h1>
